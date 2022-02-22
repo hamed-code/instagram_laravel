590532683d0f285f5b5ba186b6e5d67be66855ce
hamed-code <h.mahmoodabadi306@gmail.com>
2022-02-22 23:52:07 +0330

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+//        Blade::directive('ifGuest', function (){
+/*            return "<?php if (auth()->guest()) ?>";*/
+//        });
+        Blade::if('env', function ($env){
+            return app()->environment($env);
+        });
+
+    }
+}
